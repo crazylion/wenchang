6c542b438388644f7784ec3d0d11f1df49655dda
Meng-Yang Lee <crazylion2@gmail.com>
2012-12-31 23:37:37 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wenchang::Application.config.secret_token = 'bc95dff3b13197043d93967810fc613e32d6cb8e9479bb52be7d58d44d07956869feb180aebddd4588ae2016a843ae102d4672da062b787cdd9b7e01234b8eac'
