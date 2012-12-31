7a24ec1728d870b29e1f7ac77b824d9e931cf12d
Meng-Yang Lee <crazylion2@gmail.com>
2013-01-01 00:02:09 +0800

    add fb login to system

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.2.9'
 
 gem 'sqlite3'
 gem 'devise'
+gem 'omniauth'
+gem 'omniauth-facebook'
 
 
 # Gems used only for assets and not required
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -229,4 +229,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
+   config.omniauth :facebook,"268765596532907","07bc750affaa2903a33c02e749edf5f8" if Rails.env =='development'
 end
