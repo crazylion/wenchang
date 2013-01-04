c507df317ef35489c46558e8bb9f889a012756a8
Meng-Yang Lee <crazylion2@gmail.com>
2013-01-05 01:37:38 +0800

    基礎的頁面

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Wenchang::Application.routes.draw do
+  get "users/show"
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Wenchang::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'users#show'
 
   # See how all your routes lay out with "rake routes"
 
